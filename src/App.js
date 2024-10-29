5afe87eff88f6b20400f69196a4b0b9028fa7cc1
anilkumarswain140 <anil.swain@mindfiresolutions.com>
2024-10-29 13:37:38 +0530

    fix: add fix for header footer visibility

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-// import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import SignUp from './pages/Signup/SingUp';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/LogInForm';
 import RecipeDetails from './pages/RecipeDetailsPage/RecipeDetailsPage';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === '/' || location.pathname === '/login';
+
   return (
     <>
+      {!hideHeaderFooter && <Header />}
       <Routes>
-        <Route path="/" element={< SignUp />} />
-        <Route path="login" element={< Login />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="recipedetails/:id" element={<RecipeDetails />} />
       </Routes>
+      {!hideHeaderFooter && <Footer />}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,16 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import Footer from './components/Footer/footer';
-import Header from './components/Header/header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <Header/>
       <App />
-      <Footer/>
     </BrowserRouter>
   </Provider>
 </React.StrictMode>,
